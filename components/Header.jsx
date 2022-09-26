a5b9b074dea25bc6c4dc38b262763a79a898b1ae
Alex White <alex.white@hey.com>
2022-09-26 13:48:50 -0400

    First Version

---
+++ components/Header.jsx
@@ -0,0 +1,283 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from "react";
+import Image from 'next/future/image'
+import { Popover, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  ChatBubbleBottomCenterTextIcon,
+  ChatBubbleLeftRightIcon,
+  InboxIcon,
+  QuestionMarkCircleIcon,
+  XMarkIcon,
+  CameraIcon,
+  CubeIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import Logo from "./Logo";
+import Background from "../images/samples/cta-sample.jpg";
+
+const solutions = [
+  {
+    name: "Photography",
+    description:
+      "Showcase the best angles of your property.",
+    href: "#",
+    icon: CameraIcon,
+  },
+  {
+    name: "Video",
+    description:
+      "Walk through videos with optional narration.",
+    href: "#",
+    icon: VideoCameraIcon,
+  },
+  {
+    name: "3D Scans",
+    description: "Offer interactive virtual tours.",
+    href: "#",
+    icon: CubeIcon,
+  },
+];
+const navigation = [
+  { name: "Book Now", href: "/book" },
+  { name: "Free Quote", href: "/quote" },
+  { name: "About Us", href: "/about" },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+  return (
+    <div className="bg-white">
+      <header>
+        <Popover className="relative bg-white">
+          <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <a href="#">
+                <span className="sr-only">
+                  Perfect Angle Real Estate Photography
+                </span>
+                <Logo className="h-8 w-auto sm:h-10"></Logo>
+              </a>
+            </div>
+            <div className="-my-2 -mr-2 md:hidden">
+              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span className="sr-only">Open menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+            <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className={classNames(
+                        open ? "text-gray-900" : "text-gray-500",
+                        "group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      )}
+                    >
+                      <span>Services</span>
+                      <ChevronDownIcon
+                        className={classNames(
+                          open ? "text-gray-600" : "text-gray-400",
+                          "ml-2 h-5 w-5 group-hover:text-gray-500"
+                        )}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform lg:left-1/2 lg:ml-0 lg:max-w-2xl lg:-translate-x-1/2">
+                        <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
+                            {solutions.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                              >
+                                <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-600 text-white sm:h-12 sm:w-12">
+                                  <item.icon
+                                    className="h-6 w-6"
+                                    aria-hidden="true"
+                                  />
+                                </div>
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-900">
+                                    {item.name}
+                                  </p>
+                                  <p className="mt-1 text-sm text-gray-500">
+                                    {item.description}
+                                  </p>
+                                </div>
+                              </a>
+                            ))}
+                          </div>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-base font-medium text-gray-500 hover:text-gray-900"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </Popover.Group>
+            <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+            </div>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="duration-200 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute inset-x-0 top-0 z-30 origin-top-right transform p-2 transition md:hidden"
+            >
+              <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="px-5 pt-5 pb-6">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Close menu</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="mt-6">
+                    <nav className="grid grid-cols-1 gap-7">
+                      {solutions.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="-m-3 flex items-center rounded-lg p-3 hover:bg-gray-50"
+                        >
+                          <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-600 text-white">
+                            <item.icon className="h-6 w-6" aria-hidden="true" />
+                          </div>
+                          <div className="ml-4 text-base font-medium text-gray-900">
+                            {item.name}
+                          </div>
+                        </a>
+                      ))}
+                    </nav>
+                  </div>
+                </div>
+                <div className="py-6 px-5">
+                  <div className="grid grid-cols-2 gap-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="text-base font-medium text-gray-900 hover:text-gray-700"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="mt-6">
+                    <a
+                      href="#"
+                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                    >
+                      Sign up
+                    </a>
+                    <p className="mt-6 text-center text-base font-medium text-gray-500">
+                      Existing customer?
+                      <a href="#" className="text-gray-900">
+                        Sign in
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </header>
+
+      <main>
+        <div>
+          {/* Hero card */}
+          <div className="relative">
+            <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 pb-16">
+              <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+                <div className="absolute inset-0">
+                  <Image
+                    src={Background}
+                    alt="A single family home"
+                    className="h-full w-full object-cover"
+                  />
+                  <div className="absolute inset-0 bg-gray-500 mix-blend-multiply" />
+                </div>
+                <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+                  <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                    <span className="inline-block text-white">We add the</span>
+                    {"  "}
+                    <span className="text-indigo-200">wow factor</span>
+                    <span className="block text-white">to your property</span>
+                  </h1>
+                  <p className="mx-auto mt-6 max-w-lg text-center text-xl text-indigo-200 sm:max-w-3xl">
+                    Sell or rent faster with beautiful photos for your MLS
+                    listing, or reach new customers and professionally showcase
+                    your business online.
+                  </p>
+                  <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                    <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                      <a
+                        href="#"
+                        className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-indigo-700 shadow-sm hover:bg-indigo-50 sm:px-8"
+                      >
+                        Book Now
+                      </a>
+                      <a
+                        href="#"
+                        className="flex items-center justify-center rounded-md border border-transparent bg-indigo-500 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8"
+                      >
+                        Get a Quote
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* More main page content here... */}
+      </main>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,204 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import {
+  CubeIcon,
+  ChatBubbleBottomCenterTextIcon,
+  CameraIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/24/outline";
+
+import Gallery1 from "../images/samples/gallery-1.jpg";
+import Gallery2 from "../images/samples/gallery-2.jpg";
+import Gallery3 from "../images/samples/gallery-3.jpg";
+import Gallery4 from "../images/samples/gallery-4.jpg";
+
+function QuoteCTASection() {
+  return (
+    <div className="bg-indigo-700 my-16">
+      <div className="mx-auto max-w-2xl py-16 px-4 text-center sm:py-20 sm:px-6 lg:px-8">
+        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <span className="block">Get a free quote today.</span>
+        </h2>
+        <p className="mt-4 text-lg leading-6 text-indigo-200">
+          No obligation, no pressure. Contact us to discuss which services are best for you and to get a free quote.
+        </p>
+        <a
+          href="#"
+          className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50 sm:w-auto"
+        >
+          Get a free quote
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function ServicesSection() {
+  const features = [
+    {
+      name: "Photography",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: CameraIcon,
+    },
+    {
+      name: "Videography",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: VideoCameraIcon,
+    },
+    {
+      name: "3D Scanning",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: CubeIcon,
+    },
+  ];
+
+  return (
+    <div className="bg-white py-12">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-lg font-semibold text-indigo-600">
+            What we offer
+          </h2>
+          <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            We offer a step above the competition.
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Our professional services can take your real estate listing, rental
+            listing or business ad to the next level.
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <feature.icon
+                      className="h-6 w-6"
+                      aria-hidden="true"
+                    ></feature.icon>
+                  </div>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
+                    {feature.name}
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CTASection() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-lg font-semibold text-indigo-600">
+            More than a pretty picture
+          </h2>
+          <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
+            First impressions do matter.
+          </p>
+          <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
+            83% of buyers have said photographs are exteremly important when
+            deciding to view a property. That's why it's important that you put
+            your best foot forward with stunning, professional photographs,
+            videos and 3D scans.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ImageGrid() {
+  return (
+    <section class="overflow-hidden text-gray-700 w-full">
+      <div class="mb-16 px-5 py-2 mx-auto  lg:px-32">
+        <div class="flex flex-col lg:flex-row lg:flex-wrap -m-1 md:-m-2">
+          <div class="flex flex-col lg:flex-row lg:flex-wrap w-full lg:w-1/2">
+            <div class="w-full lg:w-1/2 p-1 md:p-2">
+              <Image
+                src={Gallery1}
+                alt="A single family home"
+                className="block object-cover object-center w-full h-full rounded-lg"
+              />
+            </div>
+            <div class="w-full lg:w-1/2 p-1 md:p-2">
+              <Image
+                src={Gallery2}
+                alt="A single family home"
+                className="block object-cover object-center w-full h-full rounded-lg"
+              />
+            </div>
+            <div class="w-full p-1 md:p-2">
+              <Image
+                src={Gallery3}
+                alt="A single family home"
+                className="block object-cover object-center w-full h-full rounded-lg"
+              />
+            </div>
+          </div>
+          <div class="flex flex-col lg:flex-row lg:flex-wrap w-full lg:w-1/2">
+            <div class="w-full p-1 md:p-2">
+              <Image
+                src={Gallery4}
+                alt="A single family home"
+                className="block object-cover object-center w-full h-full rounded-lg"
+              />
+            </div>
+            <div class="w-full lg:w-1/2 p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block object-cover object-center w-full h-full rounded-lg"
+                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
+              ></img>
+            </div>
+            <div class="w-full lg:w-1/2 p-2">
+              <img
+                alt="gallery"
+                class="block object-cover object-center w-full h-full rounded-lg"
+                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Perfect Angle | Real estate photography in Columbus, Ohio</title>
+        <meta
+          name="description"
+          content="Real estate photographer in Columbus, Ohio offering photos, videos and 3D scans for residential and commerical properties."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Header></Header>
+      <CTASection></CTASection>
+      <ImageGrid></ImageGrid>
+      <ServicesSection></ServicesSection>
+      <QuoteCTASection></QuoteCTASection>
+      <Footer></Footer>
     </div>
-  )
+  );
 }
